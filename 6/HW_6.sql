2b69f3c91870001b442eedccdd0f6b8877cd4eff
VictoriaCernenchi <133024448+victoriacernenchi@users.noreply.github.com>
2023-12-14 22:37:49 +0100

    Add files via upload

---
+++ 6/HW_6.sql
@@ -0,0 +1,15 @@
+Table CustomersServices{
+  chat_id integer
+  user_id integer
+  user_name varchar(128)
+  history text
+  is_deleted bool
+  created_at timestamp
+  reaction reaction_id
+  messege integer
+  reason integer
+}
+
+
+
+https://dbdiagram.io/d/Copy-of-online_school-65564d533be1495787253458
